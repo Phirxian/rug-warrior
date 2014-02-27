dbf60cd85c072a18a273b1b5d3b72cf6f29b7aa4
ja <root@darkstar.example.net>
2014-02-27 18:16:50 +0100

    modified:   libs/shared/motor.c 	libs/behavior/wall_decrease.c 	libs/behavior/wall_scan.c

--- libs/shared/motor.c
+++ libs/shared/motor.c
@@ -210,3 +210,63 @@ void rotate(int flags, int angle)
         motor(1, _motor_right_speed_);
     }
 }
+
+void move_behind(float distance, float marging)
+{
+    int pid;
+    float last;
+    int scan;
+
+    printf("Runs until a wall is found 85cm\n");
+
+    init_motors();
+    init_sonar();
+    sonar_init_servo();
+
+    set_servo(0);
+    sleep(0.175);
+
+    start_process(encoder_aux());
+
+    while(1)
+    {
+        ping();
+        last = feetToCm(range());
+
+        printf("%f\n", last);
+
+        if(last >= (distance+marging))
+        {
+            kill_process(pid);
+            _running_process_running_ = 0;
+            _motor_initial_speed_ = 50+(int)(last-87.);
+            pid = start_process(running_forever());
+        }
+        else if(last <= (distance-marging))
+        {
+            kill_process(pid);
+            _running_process_running_ = 0;
+            _motor_initial_speed_ = -50+(int)(-83.+last);
+            pid = start_process(running_forever());
+        }
+        else if(!_motor_initial_speed_) break;
+        else break;
+
+        sleep(0.175);
+    }
+
+    _running_process_running_ = 0;
+
+    for(scan = -100; scan<100; ++scan)
+    {
+        set_servo(scan);
+        ping();
+        last = feetToCm(range());
+        printf("scan %f\n", last);
+    }
+
+    set_servo(0);
+    sonar_servo_off();
+    stop();
+}
+
