bd3f20c013d2bad5578d8e5ecd90eeaca99b49d4
ja <root@darkstar.example.net>
2014-02-27 17:50:37 +0100

    modified:   libs/behavior/wall_stop.c

--- libs/behavior/wall_stop.c
+++ libs/behavior/wall_stop.c
@@ -1,37 +1,56 @@
 void main()
 {
-    float last = 100.0;
+    int pid;
+    float last;
 
-    _motor_initial_speed_ = 100;
-
-    printf("runing while not in front of wall 50cm");
+    printf("Runs until a wall is found 85cm\n");
 
     init_motors();
     init_sonar();
     sonar_init_servo();
 
     set_servo(0);
+    sleep(0.175);
 
     start_process(encoder_aux());
-    start_process(running_forever());
 
-    while(last >= 52.)
+    while(1)
     {
         ping();
         last = feetToCm(range());
 
         printf("%f\n", last);
 
+        if(last >= 86.)
+        {
+            kill_process(pid);
+            _running_process_running_ = 0;
+            _motor_initial_speed_ = 50+(int)(last-87.);
+            pid = start_process(running_forever());
+        }
+        else if(last <= 82.)
+        {
+            kill_process(pid);
+            _running_process_running_ = 0;
+            _motor_initial_speed_ = -50+(int)(-83.+last);
+            pid = start_process(running_forever());
+        }
+        else if(!_motor_initial_speed_) break;
+        else break;
+
+
         /*
-         * more the rug-warrior is
-         * front of wall more the
-         * rug-warrior is waiting
-         * for a signals
+         * The closest the Rug-Warrior is
+         * from a wall, the more the
+         * Rug-Warrior is waiting
+         * for a signal
          */
-        sleep(0.1);
+        /*sleep(feetToMotor(cmToFeet(last - 85.), _motor_initial_speed_));*/
+        sleep(0.175);
     }
 
     _running_process_running_ = 0;
+    /*kill_process(pid);*/
     sonar_servo_off();
     stop();
 }
